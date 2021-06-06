8f7b465ac74db5459554e550a2efb834de4ed38d
Arthur Givigir <arthur316@gmail.com>
2021-06-06 17:10:41 -0300

    🐛 [CHANGED] Launch screen

---
+++ MyCoins/LaunchScreen/LaunchScreenView.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchScreenVC.swift
+//  MyCoins
+//
+//  Created by Arthur Givigir on 06/06/21.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class LaunchScreenView: UIViewController {}
+
