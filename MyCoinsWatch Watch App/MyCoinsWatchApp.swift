2de491d281f6b7a93384997dc7fac685cabd460d
Arthur Givigir <arthur316@gmail.com>
2022-10-11 13:07:00 -0300

    🚧 Added new apple watch target

---
+++ MyCoinsWatch Watch App/MyCoinsWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoinsWatchApp.swift
+//  MyCoinsWatch Watch App
+//
+//  Created by Arthur Givigir on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCoinsWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
